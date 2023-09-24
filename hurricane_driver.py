197f83c21a5a05c231c22cf938c41c1878fe2aeb
cody11allison <145852754+cody11allison@users.noreply.github.com>
2023-09-24 10:20:17 -0500

    Update hurricane_driver.py
    Testuh

--- hurricane_driver.py
+++ hurricane_driver.py
@@ -37,3 +37,36 @@ def main():
     return None
 
 main()
+
+from Hurricane import Hurricane
+from kml_export import kmlexport
+
+Calvin = Hurricane("Calvin")
+Calvin.dataPointExtraction()
+Calvin.distanceInaccuracy()
+kmlexport(Calvin.recordedPos, Calvin.projectedPos, Calvin, "Calvin")
+Don = Hurricane("Don")
+# Don.resetClass()
+Don.dataPointExtraction()
+Don.distanceInaccuracy()
+kmlexport(Don.recordedPos, Don.projectedPos, Don, "Don")
+Dora = Hurricane("Dora")
+# Dora.resetClass()
+Dora.dataPointExtraction()
+Dora.distanceInaccuracy()
+kmlexport(Dora.recordedPos, Dora.projectedPos, Dora, "Dora")
+Franklin = Hurricane("Franklin")
+# Franklin.resetClass()
+Franklin.dataPointExtraction()
+Franklin.distanceInaccuracy()
+kmlexport(Franklin.recordedPos, Franklin.projectedPos, Franklin, "Franklin")
+Lee = Hurricane("Lee")
+# Lee.resetClass()
+Lee.dataPointExtraction()
+Lee.distanceInaccuracy()
+kmlexport(Lee.recordedPos, Lee.projectedPos, Lee, "Lee")
+Margot = Hurricane("Margot")
+# # Margot.resetClass()
+Margot.dataPointExtraction()
+Margot.distanceInaccuracy()
+kmlexport(Margot.recordedPos, Margot.projectedPos, Margot, "Margot")
