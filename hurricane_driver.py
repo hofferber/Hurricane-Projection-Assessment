95127b2c0c84f43ca238db43b9fc2fbad38db8e4
hofferber <freya.hofferber@gmail.com>
2023-09-23 21:16:45 -0500

    update

--- hurricane_driver.py
+++ hurricane_driver.py
@@ -1,2 +1,18 @@
 from Hurricane import Hurricane
 from kml_export import kmlexport
+
+
+def main():
+    hurricaneList = []
+    # Replace with reading folder names in HurricaneKMLfiles folder
+    # nameList = ["Hurricane_Calvin",  "Hurricane_Don"]
+    # h = 0
+    # for i in nameList:
+    #     i = Hurricane
+    #     hurricaneList[h] = i
+    #     h += 1
+
+    Hurricane_Calvin = Hurricane
+
+    
+    
